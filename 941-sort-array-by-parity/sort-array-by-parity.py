dd169290ffdc4a29045723cf915e49db10172cec
Serzh-byte <serzhtadevosyan1@gmail.com>
2024-08-19 14:55:43 +0100

    Time: 64 ms (77.19%) | Memory: 17.3 MB (52.27%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -1,8 +1,4 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        slow = 0
-        for fast in range(len(nums)):
-            if nums[fast]%2==0:
-                nums[slow], nums[fast] = nums[fast], nums[slow]
-                slow += 1
-        return nums
+        # Separate even and odd numbers into two lists and concatenate them
+        return [x for x in nums if x % 2 == 0] + [x for x in nums if x % 2 != 0]
