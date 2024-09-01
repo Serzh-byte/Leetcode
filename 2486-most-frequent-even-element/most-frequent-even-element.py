e3c578ff78e27088fb0f27f38fab141080796c6b
Serzh-byte <serzhtadevosyan1@gmail.com>
2024-09-01 15:20:17 +0100

    Time: 202 ms (97.40%) | Memory: 16.9 MB (78.14%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.py
+++ 2486-most-frequent-even-element/most-frequent-even-element.py
@@ -1,10 +1,23 @@
+from collections import defaultdict
+from typing import List
+
 class Solution:
     def mostFrequentEven(self, nums: List[int]) -> int:
-        even_counts = {num: nums.count(num) for num in nums if num % 2 == 0}
+        count_map = defaultdict(int)
+
+        for num in nums:
+            if num % 2 == 0:
+                count_map[num] += 1
         
-        if even_counts:
-            max_value = max(even_counts.values())
-            most_frequent_evens = [key for key, value in even_counts.items() if value == max_value]
-            return min(most_frequent_evens)
-        else:
+        if not count_map:
             return -1
+
+        most_frequent = -1
+        max_count = 0
+        
+        for num, count in count_map.items():
+            if count > max_count or (count == max_count and num < most_frequent):
+                max_count = count
+                most_frequent = num
+        
+        return most_frequent
