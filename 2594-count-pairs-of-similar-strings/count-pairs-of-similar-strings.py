81c3c4375e1e1bce5bc3b6900a62e2c6c24cc504
Serzh-byte <serzhtadevosyan1@gmail.com>
2024-09-12 11:26:59 +0100

    Time: 58 ms (83.16%) | Memory: 16.5 MB (97.71%) - LeetSync

--- 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.py
+++ 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.py
@@ -2,15 +2,10 @@ class Solution:
     def similarPairs(self, words: List[str]) -> int:
         char_count = {}
         count = 0
+        
         for word in words:
-            char = frozenset(word)
-
-            if char in char_count:
-                count += char_count[char]
-                char_count[char] += 1
-            else:
-                char_count[char] = 1
+            char_set = frozenset(word)
+            count += char_count.get(char_set, 0)
+            char_count[char_set] = char_count.get(char_set, 0) + 1
         
         return count
-        
-
