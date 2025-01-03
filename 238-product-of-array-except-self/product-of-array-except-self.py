ff81ceebe81eaa8d03f2aab3d4b5cead11e4bdbe
Serzh Tadevosyan <serzhtadevosyan1@gmail.com>
2025-01-03 19:14:40 +0000

    Time: 30 ms (36.65%) | Memory: 26.1 MB (22.43%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,22 +1,3 @@
-# Brute Force Solution
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        ans = [0] * n
-
-        for i in range(n):
-            prod = 1
-            for j in range(n):
-                if i != j:
-                    prod *= nums[j]
-            ans[i] = prod
-        
-        return ans
-        # Time: O(n^2)
-        # Space: O(n)
-
-
-# Optimal Solution
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         l_mult = 1
