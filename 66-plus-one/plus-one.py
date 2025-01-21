5e0ab4587a4ddf9fc720c8330ae181242544b2f8
Serzh Tadevosyan <serzhtadevosyan1@gmail.com>
2025-01-21 19:12:23 +0000

    Time: 0 ms (100.00%) | Memory: 18 MB (5.33%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,10 +1,4 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        digits = [str(char) for char in digits]
-        number = ''.join(digits)
-        number = int(number)
-        number += 1
-        number = str(number)
-        number = list(number)
-        number = [int(char) for char in number]
-        return number
+        number = int(''.join(map(str, digits))) + 1
+        return list(map(int, str(number)))
